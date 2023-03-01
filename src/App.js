78371ec7a7517853e209d787859392f8507e6996
Martim <martim.pintao@gmail.com>
2023-03-01 13:12:52 +0000

    Create the homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import { CgShoppingCart } from "react-icons/cg";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="topsection">
+                <h1>The Gaming Store</h1>
+                <div className="navbar">
+                    <h2>Home</h2>
+                    <h2>Products</h2>
+                    <CgShoppingCart className="icon" />
+                </div>
+            </div>
+            <div className="main">
+                <p>Discover The Best Gaming Products</p>
+                <button>Shop Now</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
